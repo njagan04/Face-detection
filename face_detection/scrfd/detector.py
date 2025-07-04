ad2cfb2eb97ce031425cea93322befefc0974659
Jagan <120172819+kitten04@users.noreply.github.com>
2025-07-04 21:44:45 +0530

    updated detector

--- face_detection/scrfd/detector.py
+++ face_detection/scrfd/detector.py
@@ -285,4 +285,71 @@ class SCRFD:
 
         return bboxes, landmarks
 
-        #return torch.tensor(det), img_info, bboxes, landmarks
+    def detect_tracking(
+        self, image, thresh=0.5, input_size=(128, 128), max_num=0, metric="default"
+    ):
+        assert input_size is not None or self.input_size is not None
+        height, width = image.shape[:2]
+        img_info = {"id": 0}
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = image
+
+        input_size = self.input_size if input_size is None else input_size
+
+        im_ratio = float(image.shape[0]) / image.shape[1]
+        model_ratio = float(input_size[1]) / input_size[0]
+        if im_ratio > model_ratio:
+            new_height = input_size[1]
+            new_width = int(new_height / im_ratio)
+        else:
+            new_width = input_size[0]
+            new_height = int(new_width * im_ratio)
+        det_scale = float(new_height) / image.shape[0]
+        resized_img = cv2.resize(image, (new_width, new_height))
+        det_img = np.zeros((input_size[1], input_size[0], 3), dtype=np.uint8)
+        det_img[:new_height, :new_width, :] = resized_img
+
+        scores_list, bboxes_list, kpss_list = self.forward(det_img, thresh)
+
+        scores = np.vstack(scores_list)
+        scores_ravel = scores.ravel()
+        order = scores_ravel.argsort()[::-1]
+        bboxes = np.vstack(bboxes_list)
+        if self.use_kps:
+            kpss = np.vstack(kpss_list)
+        pre_det = np.hstack((bboxes, scores)).astype(np.float32, copy=False)
+        pre_det = pre_det[order, :]
+        keep = self.nms(pre_det)
+        det = pre_det[keep, :]
+        if self.use_kps:
+            kpss = kpss[order, :, :]
+            kpss = kpss[keep, :, :]
+        else:
+            kpss = None
+        if max_num > 0 and det.shape[0] > max_num:
+            area = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
+            img_center = image.shape[0] // 2, image.shape[1] // 2
+            offsets = np.vstack(
+                [
+                    (det[:, 0] + det[:, 2]) / 2 - img_center[1],
+                    (det[:, 1] + det[:, 3]) / 2 - img_center[0],
+                ]
+            )
+            offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
+            if metric == "max":
+                values = area
+            else:
+                values = (
+                    area - offset_dist_squared * 2.0
+                )  # some extra weight on the centering
+            bindex = np.argsort(values)[::-1]  # some extra weight on the centering
+            bindex = bindex[0:max_num]
+            det = det[bindex, :]
+            if kpss is not None:
+                kpss = kpss[bindex, :]
+
+        bboxes = np.int32(det / det_scale)
+        landmarks = np.int32(kpss / det_scale)
+
+        return torch.tensor(det), img_info, bboxes, landmarks
