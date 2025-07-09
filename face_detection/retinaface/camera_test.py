34ab0aeb4bb62badbc3e79a73d71a18636c56b17
Jagan <120172819+kitten04@users.noreply.github.com>
2025-07-09 17:05:08 +0530

    code refactorisation

--- face_detection/retinaface/camera_test.py
+++ face_detection/retinaface/camera_test.py
@@ -159,3 +159,59 @@ if __name__ == "__main__":
         scale1 = scale1.to(device)
         landms = landms * scale1 / resize
         landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > args.confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][: args.top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, args.nms_threshold)
+        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[: args.keep_top_k, :]
+        landms = landms[: args.keep_top_k, :]
+
+        dets = np.concatenate((dets, landms), axis=1)
+
+        # show image
+        if args.save_image:
+            for b in dets:
+                if b[4] < args.vis_thres:
+                    continue
+                text = "{:.4f}".format(b[4])
+                b = list(map(int, b))
+                cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+                cx = b[0]
+                cy = b[1] + 12
+                cv2.putText(
+                    img_raw,
+                    text,
+                    (cx, cy),
+                    cv2.FONT_HERSHEY_DUPLEX,
+                    0.5,
+                    (255, 255, 255),
+                )
+
+                # landms
+                # cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
+                # cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
+                # cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
+                # cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
+                # cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
+
+            # save image
+            name = "test.jpg"
+            cv2.imwrite(name, img_raw)
+            cv2.imshow("camera", img_raw)
